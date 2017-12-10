dd383166996ab320e98314cabc869a5f53228499
CEM SINAN OZTURK <cemsinan@Cems-MBP-2.hitronhub.home>
2017-12-10 12:18:32 -0800

    modify run_all script to be able to run all scripts

--- run_all.sh
+++ run_all.sh
@@ -2,4 +2,9 @@
 
 ## add data into project:
 
-Rscript src/bindSave.R "https://www.quandl.com/api/v3/datasets/CBRT/TP_FG_TG01.csv?api_key=tPKkRzbE46iPtm71hJM8&collapse=monthly" "https://www.quandl.com/api/v3/datasets/BOE/XUMLBK75.csv?api_key=tPKkRzbE46iPtm71hJM8&collapse=monthly" cpiFx
+Rscript src/SaveClean.R "https://www.quandl.com/api/v3/datasets/CBRT/TP_FG_TG01.csv?api_key=tPKkRzbE46iPtm71hJM8&collapse=monthly" "https://www.quandl.com/api/v3/datasets/BOE/XUMLBK75.csv?api_key=tPKkRzbE46iPtm71hJM8&collapse=monthly" allData
+Rscript src/Graphs.R results/allData Trend
+Rscript src/Analysis.R results/allData linearModelResults
+
+# create report
+Rscript -e 'ezknitr::ezknit("src/Project_Report.Rmd", out_dir = "docs")'
